4eefa98f13d680cb3ca2aeee729dd77854b06124
adrianapikaljov <pikaljovadri@gmail.com>
2025-12-04 10:05:12 +0200

    ul tunniplaan on lisatud

---
+++ content/mobiilimall/kolmapaev.php
@@ -0,0 +1,10 @@
+<?php require("p2is.php"); ?>
+    <h2>Kolmapäev</h2>
+
+    <ol>
+        <li>Matemaatika</li>
+        <li>Emakeel</li>
+        <li>Kehaline kasvatus</li>
+    </ol>
+
+<?php require("jalus.php"); ?>
