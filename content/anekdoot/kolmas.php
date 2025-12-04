4d975c2c60f366094283da0e5af251c3a410b733
adrianapikaljov <pikaljovadri@gmail.com>
2025-12-04 10:07:17 +0200

    ul anekdoot on loodud

---
+++ content/anekdoot/kolmas.php
@@ -0,0 +1,15 @@
+<?php require("anekdotleht.php"); ?>
+    <div id="anekdot">
+        <h2>Kolmas</h2>
+
+        <ul>
+            <li>Kohv keeldus inimest äratamast, sest „ma pole siin imetegija, ma olen lihtsalt jook“.</li>
+
+        </ul>
+
+    </div>
+
+<?php
+//jalus
+include("jalus.php");
+?>
---
+++ content/anekdoot/nav.php
@@ -0,0 +1,10 @@
+<div id="header">
+    <div class="nav">
+        <ul>
+            <li><a href="esimene.php">1. nali</a></li>
+            <li><a href="teine.php">2. nali</a></li>
+            <li><a href="kolmas.php">3. nali</a></li>
+            <li><a href="https://adrianapikaljov24.thkit.ee/webPHP/index.php?link=avaleht.php">Tagasi</a></li>
+        </ul>
+    </div>
+</div><?php
